6a1b106a78209ae366e9c540337380a0e47fca06
Tian Tian <ttian1@lor.lti.cs.cmu.edu>
2016-09-19 21:57:24 -0400

    edit run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,1 +1,6 @@
+export PATH=/home/ubuntu/venv/bin:/usr/local/cuda/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda:/usr/local/cuda/lib64:/opt/OpenBLAS-no-openmp/lib
 THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python graph2vec.py
+
+
+
